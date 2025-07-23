59337a6b456a14b32d8cc306d53b6b3aa26c83aa
Rahul Mali <rahulvm1.46@gmail.com>
2025-07-24 01:51:44 +0530

    message

---
+++ .next/static/fPEHEzDOrHvuJ1TdVr72u/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-9fd5cb5f6e485cc5.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
